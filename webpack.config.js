3c73e865cab5f3fcbc058cb9e51c2ab13e6c797b
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 15:51:29 +0100

    change index.js directory

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
    entry: "./src/index.ts",
    output: {
        filename: "index.js",
-       path: path.resolve(__dirname)
+       path: path.resolve(__dirname, "dist")
    },
    resolve: {
        extensions: [".webpack.js", ".web.js", ".ts", ".js"]
