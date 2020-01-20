7e3e464dda65f9568c78bd292460003049d10293
BuildTools <unconfigured@null.spigotmc.org>
2020-01-20 18:12:21 +0100

    done

---
+++ dist/types.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EEvery;
+(function (EEvery) {
+    EEvery["TYP"] = "every";
+    EEvery["SECOND"] = "second";
+    EEvery["MINUTE"] = "minute";
+    EEvery["HOUR"] = "hour";
+    EEvery["DAY"] = "day";
+    EEvery["WEEK"] = "week";
+    EEvery["MONTH"] = "month";
+})(EEvery = exports.EEvery || (exports.EEvery = {}));
+var EStatics;
+(function (EStatics) {
+    EStatics["TYP"] = "static";
+    EStatics["SECOND"] = "second";
+    EStatics["MINUTE"] = "minute";
+    EStatics["HOUR"] = "hour";
+    EStatics["DAY"] = "day";
+    EStatics["WEEK"] = "week";
+    EStatics["MONTH"] = "month";
+})(EStatics = exports.EStatics || (exports.EStatics = {}));
+var ETyp;
+(function (ETyp) {
+    ETyp["STATIC"] = "static";
+    ETyp["SCHEDULED"] = "scheduled";
+})(ETyp = exports.ETyp || (exports.ETyp = {}));
+//# sourceMappingURL=types.js.map
